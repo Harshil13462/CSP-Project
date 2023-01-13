ff0023245b8fb2a957d69e9f897aad340ce24181
Harshil Pathri <harshil@Harshils-MacBook-Pro.local>
2023-01-12 18:34:20 -0800

    yay

--- test.py
+++ test.py
@@ -9,10 +9,17 @@ WIDTH = 1080
 HEIGHT = 840
 
 
-INVADER_HEIGHT = 25
-INVADER_LENGTH = 75
+INVADER_HEIGHT = 37
+INVADER_LENGTH = 42
 
 INVADER_GAP = 25
+pygame.init()
+
+LOSE = pygame.mixer.Sound("mixkit-player-losing-or-failing-2042.wav")
+SHOOT = pygame.mixer.Sound("mixkit-video-game-retro-click-237.wav")
+pygame.mixer.music.load('music.wav')
+pygame.mixer.music.play(-1)
+HIT = pygame.mixer.Sound("mixkit-fast-game-explosion-1688.wav")
 
 
 BULLET_LENGTH = 5
@@ -61,9 +68,11 @@ class Bullet(pygame.sprite.Sprite):
 			if type(self.target) == list:
 				for i in self.target:
 					if self.x > i.x - BULLET_LENGTH and self.x < i.x + DEFENDER_LENGTH and self.y + BULLET_HEIGHT > i.y and self.y < i.y + DEFENDER_HEIGHT:
+						pygame.mixer.Sound.play(HIT)
 						return i
 			else:
 				if self.x > self.target.x - BULLET_LENGTH and self.x < self.target.x + DEFENDER_LENGTH and self.y + BULLET_HEIGHT > self.target.y and self.y < self.target.y + DEFENDER_HEIGHT:
+					pygame.mixer.Sound.play(HIT)
 					self.target.lives -= 1
 					return 1
 			self.screen.blit(self.surf, (self.x, self.y))
@@ -80,12 +89,14 @@ class Invader(Character):
 	def __init__(self, x, y, screen, variables):
 		super().__init__(x, y, screen, 1)
 		self.team = 2
-		self.surf = pygame.Surface((INVADER_LENGTH, INVADER_HEIGHT))
-		self.surf.fill((255, 255, 255))
+		self.img = pygame.image.load("invader.jpg")
+		self.img = pygame.transform.rotozoom(self.img, 0, 0.25)
+		# self.surf = pygame.Surface((INVADER_LENGTH, INVADER_HEIGHT))
+		# self.surf.fill((255, 255, 255))
 		self.variables = variables
 		self.prev = 1
     
-	def update(self):
+	def update(self, level):
 		if self.prev != variables[0]:
 			self.y = self.y + INVADER_HEIGHT + INVADER_GAP
 		self.x = self.x + variables[0]
@@ -93,13 +104,13 @@ class Invader(Character):
 			variables[0] *= -1
 			self.y = self.y + INVADER_HEIGHT + INVADER_GAP
 		self.prev = variables[0]
-		self.screen.blit(self.surf, (self.x, self.y))
+		self.screen.blit(self.img, (self.x, self.y))
+		# self.screen.blit(self.surf, (self.x, self.y))
+		if self.y > 800:
+			return 1
 
-		if random.randint(1, 1000) == 1:
+		if random.randint(1, 10000) <= level:
 			createBullet(self.x + INVADER_LENGTH / 2, self.y + INVADER_HEIGHT, self.screen, self.team)
-
-	def shoot(self):
-		pass
 
 class Defender(Character):
 	def __init__(self, x, y, screen):
@@ -117,12 +128,11 @@ class Defender(Character):
 			self.x = self.x + 5
 			if self.x > WIDTH - DEFENDER_LENGTH:
 				self.x = WIDTH - DEFENDER_LENGTH
-		if pressed_keys[K_SPACE] and time.time() - self.time > 0.5:
+		if pressed_keys[K_SPACE] and time.time() - self.time > 0.4:
+			pygame.mixer.Sound.play(SHOOT)
 			createBullet(self.x + DEFENDER_LENGTH / 2, self.y + DEFENDER_HEIGHT - BULLET_HEIGHT, self.screen, self.team)
 			self.time = time.time()
 		self.screen.blit(self.surf, (self.x, self.y))
-
-pygame.init()
 
 fps = 60
 fpsClock = pygame.time.Clock()
@@ -135,6 +145,11 @@ player = Defender(30, 800, screen)
 gameOver = False
 f = open("highScore.txt", "r")
 highScore = f.readline()
+if not highScore:
+	f.close()
+	f = open("highScore.txt", "w")
+	f.write("0")
+	highScore = 0
 f.close()
 newHighScore = False
 while running:
@@ -143,10 +158,6 @@ while running:
 		if event.type == pygame.KEYDOWN:
 			if event.key == K_ESCAPE:
 				running = False
-			# if event.key == K_TAB:
-			# 	fps = 240
-			# else:
-			# 	fps = 60
 			if event.key == K_r and gameOver:
 				gameOver = False
 				score = 0
@@ -196,8 +207,13 @@ while running:
 	if len(invaders) == 0:
 		resetInvaders()
 	pressed_keys = pygame.key.get_pressed()
+	level = min([15, score // 20 + 1])
 	for i in invaders:
-		i.update()
+		if i.update(level) == 1:
+			lives = 0
+			gameOver = True
+			pygame.mixer.Sound.play(LOSE)
+			continue
 	for i in bullets:
 		x = i.update()
 		if x:
@@ -223,6 +239,7 @@ while running:
 
 	if player.lives == 0:
 		gameOver = True
+		pygame.mixer.Sound.play(LOSE)
 	player.update(pressed_keys)
 
 	pygame.display.flip()
